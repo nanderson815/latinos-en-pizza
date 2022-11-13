c6c3b15d7263627bde98c2cc1ee6247e3986416f
Noah Anderson <nanderson815@gmail.com>
2022-11-13 11:40:37 -0500

    trying to fix form

--- app/routes/contactUs.tsx
+++ app/routes/contactUs.tsx
@@ -1,8 +1,23 @@
 import Button from "~/components/shared/button";
 import Footer from "~/components/shared/footer";
 import Header from "~/components/shared/header";
+import type { DataFunctionArgs } from "@remix-run/node"
+
+export async function action({ request }: DataFunctionArgs) {
+    const data: any = await request.formData();
+    console.log(new URLSearchParams(data).toString());
+    console.log(request.url);
+    const resp = await fetch(`${request.url}/form`, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(data).toString(),
+    })
+    console.log(resp);
+    return null;
+}
 
 export default function Contact() {
+
     const handleSubmit = (event: any) => {
         console.log(event);
         event.preventDefault();
@@ -21,6 +36,7 @@ export default function Contact() {
             })
             .catch((error) => alert(error));
     };
+
     return (
         <div className="flex flex-col justify-between h-[100vh]">
             <Header />
@@ -28,7 +44,8 @@ export default function Contact() {
                 <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Us</h2>
                     <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">Have a question or feedback? Want YOM Ice Cream at your next event? We’d love to hear from you!</p>
-                    <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" className="space-y-8" onSubmit={handleSubmit}>
+                    <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" className="space-y-8">
+                        <input type="hidden" name="form-name" value="contact" />
                         <p className="hidden">
                             <label>
                                 Don’t fill this out if you’re human: <input name="bot-field" />
@@ -36,15 +53,15 @@ export default function Contact() {
                         </p>
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                            <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="name@yomicecream.com" required />
+                            <input type="email" id="email" name="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="name@yomicecream.com" required />
                         </div>
                         <div>
                             <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-900">Subject</label>
-                            <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Let us know how we can help you" required />
+                            <input type="text" id="subject" name="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500" placeholder="Let us know how we can help you" required />
                         </div>
                         <div className="sm:col-span-2">
                             <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900">Your message</label>
-                            <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..." required></textarea>
+                            <textarea id="message" name="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500" placeholder="Leave a comment..." required></textarea>
                         </div>
                         <Button type="submit" text="Send Message" />
                     </form>
