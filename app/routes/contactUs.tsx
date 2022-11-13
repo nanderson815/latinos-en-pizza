9a0668c1719ff0086e15ed493a4ef38b2aed1c4f
Noah Anderson <nanderson815@gmail.com>
2022-11-13 12:01:47 -0500

    giving up soon

--- app/routes/contactUs.tsx
+++ app/routes/contactUs.tsx
@@ -25,7 +25,7 @@ export default function Contact() {
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Us</h2>
                     <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">Have a question or feedback? Want YOM Ice Cream at your next event? We’d love to hear from you!</p>
                     <form name="contact" method="POST" data-netlify="true" className="space-y-8">
-                        <input type="hidden" name="form-name" value="contact" />
+                        <input type="hidden" name="form-name" value="contact"></input>
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
                             <input type="email" id="email" name="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5" placeholder="name@yomicecream.com" required />
