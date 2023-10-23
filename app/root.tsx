b575bef9c3d8ea2be8d4b561d6906154c8bd7394
Noah Anderson <nanderson815@gmail.com>
2023-10-23 19:24:09 -0400

    first commit

--- app/root.tsx
+++ app/root.tsx
@@ -3,7 +3,6 @@ import styles from "./styles/global.css";
 import calendar from "./styles/calendar.css";
 import tailwind from "./styles/tailwind.css";
 import {
-  Link,
   Links,
   LiveReload,
   Meta,
@@ -12,13 +11,12 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Yom Ice Cream",
-  url: "https://www.yomicecream.com",
-  description: "YOM is premium custard ice cream perfected over 15 years in the heart of Atlanta. We believe in treating your taste buds like they’re your buds. Because life’s better making memories together, one scoop at a time.",
-  image: "http://static1.squarespace.com/static/5dd3f3cdeeffcb7babf1c291/t/5df9020b4b25696a015252f3/1576600078052/yo%CC%84m_thumbnail-18.png?format=1500w",
+  title: "Latinos En Pizza",
+  url: "https://www.latinosenpizza.org",
+  description: "Latinos En Pizza - coming soon!",
+  // image: "http://static1.squarespace.com/static/5dd3f3cdeeffcb7babf1c291/t/5df9020b4b25696a015252f3/1576600078052/yo%CC%84m_thumbnail-18.png?format=1500w",
   viewport: "width=device-width,initial-scale=1",
 });
 
@@ -45,4 +43,4 @@ export default function App() {
       </body>
     </html>
   );
-};
+}
